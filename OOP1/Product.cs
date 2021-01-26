f8467adc151fa9700f143c997d21a73fb8071c50
Burak Yoksul <359144@ogr.ktu.edu.tr>
2021-01-26 21:54:06 +0300

    26 Ocak Ders Sonu.

---
+++ OOP1/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//Sadece Özeliikler..
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public double UnıtPrice { get; set; }
+        public int UnıtsInStock { get; set; }
+
+
+    }
+}
